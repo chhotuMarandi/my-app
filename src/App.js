266d0472a24331fc0bd00b152e38f3fef92627fe
chhotu marandi <cmarandi29@gmail.com>
2023-06-23 19:23:12 +0530

    introduction to react

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-import "./App.css";
+import './App.css';
+
+let firstName = 'chhotu';
+let className = 'App-header';
 
 function App() {
+  console.log('App');
   return (
     <div className='App'>
-      
+      <div className={className}>{firstName}</div>
+      <Demo />
+    </div>
+  );
+}
+
+function Demo() {
+  console.log('Demo');
+  return (
+    <div className='App'>
+      <div className='App-header'>Demo</div>
     </div>
   );
 }
