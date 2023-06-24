f34906dafee3c7b956cc9b9962105d8eb449d69c
chhotu marandi <cmarandi29@gmail.com>
2023-06-25 01:19:24 +0530

    pass props in a component by spread operator

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import Video from './components/Video'
 
 function App() { 
+
+  let obj = {
+    title:'React js tutorial' ,
+      channel:'Coding addict',
+      views:'128k',
+      time:'8 month ago'
+  }
+
  return (
   <div>
     <h1>Hello</h1>
-    <Video  
-      title='React js tutorial' 
-      channel='Coding addict'
-      views='128k'
-      time='8 month ago'
-    />
+    <Video {...obj}/>
   </div>
  )
 }
