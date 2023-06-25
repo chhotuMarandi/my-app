28f05406ce763545819bf24dac9cfeb86d456c81
chhotu marandi <cmarandi29@gmail.com>
2023-06-25 15:24:38 +0530

    iterate videos card by map function

--- src/App.js
+++ src/App.js
@@ -3,34 +3,53 @@ import Video from './components/Video'
 
 function App() { 
 
-  let obj = {
-    title:'React js tutorial' ,
-      channel:'Coding addict',
-      views:'128k',
-      time:'8 month ago',
-      image: 'https://repository-images.githubusercontent.com/37153337/9d0a6780-394a-11eb-9fd1-6296a684b124',
-      verified: true
-  }
+  let videos = [
+    {
+      title: 'React js tutorial',
+      channel: 'Coding addict',
+      views: '128k',
+      time: '8 month ago',
+      image:
+        'https://repository-images.githubusercontent.com/37153337/9d0a6780-394a-11eb-9fd1-6296a684b124',
+      verified: true,
+    },
+    {
+      title: 'Node js tutorial',
+      channel: 'programming Yt',
+      views: '1M',
+      time: '1 year ago',
+      image:
+        'https://litslink.com/wp-content/uploads/2020/12/node.js-logo-image.png',
+      verified: true,
+    },
+    {
+      title: 'Javascript js tutorial',
+      channel: 'Codeforce',
+      views: '2M',
+      time: '2 year ago',
+      image:
+        'https://4.bp.blogspot.com/-s2EhTt57oeU/XHtQtO1QNLI/AAAAAAAANW8/KYkPQEZUyocSpA2RzqCcVt31imXPi63RACLcBGAs/s1600/Free%2BCourses%2Bto%2Blearn%2BJavaScript.jpg',
+      verified: false,
+    },
+  ];
 
  return (
    <div className='App'>
-     <Video {...obj} />
-     <Video
-       title='Node js tutorial'
-       channel='Coding addict'
-       views='1M'
-       time='1 year ago'
-       image='https://webmantra.net/BlogImages/advantages-of-nodejs.jpg'
-       verified={false}
-     />
-     <Video
-       title='javascript js tutorial'
-       channel='Coding addict'
-       views='2M'
-       time='2 year ago'
-       image='https://4.bp.blogspot.com/-s2EhTt57oeU/XHtQtO1QNLI/AAAAAAAANW8/KYkPQEZUyocSpA2RzqCcVt31imXPi63RACLcBGAs/s1600/Free%2BCourses%2Bto%2Blearn%2BJavaScript.jpg'
-       verified={true}
-     />
+     {
+      videos.map((video)=>{
+       return (
+         <Video
+           title={video.title}
+           channel={video.channel}
+           views={video.views}
+           time={video.time}
+           image={video.image}
+           verified={false}
+         />
+       )
+      })
+     }
+     
    </div>
  );
 }
