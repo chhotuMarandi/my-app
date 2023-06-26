dddd197554bfec0a2aaab69614fe151da235ca07
chhotu marandi <cmarandi29@gmail.com>
2023-06-26 22:18:44 +0530

    added play button component

---
+++ src/components/PlayButton.js
@@ -0,0 +1,15 @@
+import './PlayButton.css'
+
+function PlayButton () {
+
+ const eventHandler = () => {
+  
+ }
+ return (
+  <div className="playBtn">
+     <button onClick={eventHandler}>play</button>
+  </div>
+ )
+}
+
+export default PlayButton
