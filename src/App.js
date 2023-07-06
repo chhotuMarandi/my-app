cf570335a477def4b7f5309793674ea147fe2ce5
chhotu marandi <cmarandi29@gmail.com>
2023-07-06 13:17:00 +0530

    video card layout and style

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import './App.css'
+
+function App(){
+  return (
+    <div className="video-card">
+      <img
+        src='https://loremflickr.com/320/240/random'
+        className='thumbnail'
+        alt=''
+      />
+      <section className='details-section'>
+        <img
+          src='https://loremflickr.com/40/40/people'
+          className='profile'
+          alt=''
+        />
+
+        <div className='details'>
+          <p className='title'>React Js Tutorial and javascript</p>
+          <p className='channel'>Coder Dost</p>
+          <div className="views-time">
+            <p className='views'>6M views</p>
+            <p>•</p>
+            <p className='time'>5 month ago</p>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default App
