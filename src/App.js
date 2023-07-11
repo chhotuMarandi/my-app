36f2ad646f63a571adb47243acb08842f76b8561
chhotu marandi <cmarandi29@gmail.com>
2023-07-11 14:40:09 +0530

    add a button for add video

--- src/App.js
+++ src/App.js
@@ -7,43 +7,61 @@ import { useState } from 'react';
 
 function App(){
 
+
+
   const [videoCard, setVideoCard] = useState(videoData);
 
+  // const initialData = {
+  //   id: {id:videoCard.length + 1},
+  //   title: 'Node js Tutorial',
+  //   channel: 'Coding addict',
+  //   views: '128k',
+  //   time: '3 year ago',
+  //   verified: true,
+  // };
+
+  function addVideo(){
+    setVideoCard([
+      ...videoCard,
+      {
+        id: videoCard.length + 1 ,
+          title: 'Node js Tutorial',
+          channel: 'Coding addict',
+          views: '128k',
+          time: '3 year ago',
+          verified: true,
+      },
+    ]);
+    console.log(videoCard);
+  }
 
 
   return (
-    
-      <div className='App'>
-        {videoData.map((video) => {
-          return (
-            <Video
-              title={video.title}
-              channel={video.channel}
-              views={video.views}
-              time={video.time}
-              thumbnailImg={video.thumbnailImg}
-              profileImg={video.profileImg}
-              id={video.id}
-              key={video.id}
-              verified={video.verified}
+    <div className='App'>
+      {videoCard.map((video) => {
+        return (
+          <Video
+            title={video.title}
+            channel={video.channel}
+            views={video.views}
+            time={video.time}
+            thumbnailImg={video.thumbnailImg}
+            profileImg={video.profileImg}
+            id={video.id}
+            key={video.id}
+            verified={video.verified}
+          >
+            <PlayButton
+              onPlay={() => console.log('playing', video.title)}
+              onPause={() => console.log('paused', video.title)}
             >
-              <PlayButton
-                onPlay={() => console.log('playing', video.title)}
-                onPause={() => console.log('paused', video.title)}
-                message='playing'
-              >
-                {/* {video.title} */}
-              </PlayButton>
-            </Video>
-          );
-        })}
-        <button onClick={()=>{
-          setVideoCard([])
-        // console.log(videoCard)
-          console.log(videoCard);
-        }
-        }>Add Video</button>
-      </div>
+              {/* {video.title} */}
+            </PlayButton>
+          </Video>
+        );
+      })}
+      <button onClick={addVideo}>Add Video</button>
+    </div>
   );
 }
 
